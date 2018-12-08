6c3ae3943e3571b408d7c57b9630b8048c8d4ebf
sago007 <poul@poulsander.com>
2018-12-08 15:48:49 +0100

    Now also check date on the UI3 directory

--- gamecode_build_script.bash
+++ gamecode_build_script.bash
@@ -4,44 +4,46 @@ set -e
 cd /working
 
 git clone https://github.com/OpenArena/gamecode.git
+git clone https://github.com/OpenArena/UI3.git
 cd gamecode
 
-VERSION_ID=$(git log -1 --date=short --format="openarena_gamecode_%cd_%H")
+VERSION_ID=$(git log -1 --date=short --format="openarena_gamecode_%cd_%h")
 PKDIR=oax
 MISSIONPACKDIR=oax_m
 mkdir -p "/working/$PKDIR"
 mkdir -p "/working/$MISSIONPACKDIR"
+cd /working/UI3
+VERSION_ID_M="${VERSION_ID}$(git log -1 --date=short --format="_%cd_%h")"
 
 ZIPFILE="/data/${VERSION_ID}.zip"
-ZIPFILE_M="/data/${VERSION_ID}_m.zip"
+ZIPFILE_M="/data/${VERSION_ID_M}_m.zip"
 
-if [ -e "$ZIPFILE" ]
+if [ -e "$ZIPFILE_M" ]
 then
- echo "$ZIPFILE exist -> UP TO DATE"
+ echo "$ZIPFILE_M exist -> UP TO DATE"
  exit 1
 fi
-
-cd /working
-
-git clone https://github.com/OpenArena/UI3.git
 
 cd /working/gamecode
 
 make
-cd build/release-linux-x86_64/oax
-zip -r /working/$PKDIR/oax.pk3 vm 
-cd /extra2pk3
-zip -r /working/$PKDIR/oax.pk3 *
-cd /working
-zip -r "$ZIPFILE" "$PKDIR"
 
-cd /working/gamecode/build/release-linux-x86_64/missionpack
-zip -r /working/$MISSIONPACKDIR/oax_m1.pk3 vm
-cd /extra2pk3
-zip -r /working/$MISSIONPACKDIR/oax_m1.pk3 *
-cd /working/UI3
-zip -r /working/$MISSIONPACKDIR/oax_m0.pk3 *
-cd /working
-zip -r "$ZIPFILE_M" "$MISSIONPACKDIR"
+if [ ! -e "$ZIPFILE" ]
+then
+      cd build/release-linux-x86_64/oax
+      zip -r /working/$PKDIR/oax.pk3 vm
+      cd /extra2pk3
+      zip -r /working/$PKDIR/oax.pk3 *
+      cd /working
+      zip -r "$ZIPFILE" "$PKDIR"
+fi
 
-
+if [ ! -e "$ZIPFILE_M" ]
+then
+      cd /working/gamecode/build/release-linux-x86_64/missionpack
+      zip -r /working/$MISSIONPACKDIR/oax_m1.pk3 vm
+      cd /working/UI3
+      zip -r /working/$MISSIONPACKDIR/oax_m0.pk3 *
+      cd /working
+      zip -r "$ZIPFILE_M" "$MISSIONPACKDIR"
+fi
